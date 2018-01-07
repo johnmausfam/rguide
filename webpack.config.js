1a058353e537033b340b563313799fdb7f4f9b2e
fam <fam1923@gmail.com>
2018-01-07 21:05:36 +0800

    install typescript

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     bundle: [
       'webpack-dev-server/client?http://localhost:3000/',
       'webpack/hot/dev-server',
-      path.resolve(__dirname, 'src/index.js')
+      path.resolve(__dirname, 'src/index.tsx')
     ]
   },
   output: {
@@ -16,5 +16,23 @@ module.exports = {
   plugins:[
   	new webpack.HotModuleReplacementPlugin(),
   	new webpack.SourceMapDevToolPlugin()
-  ]
+  ],
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+  }
+  /*
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+  */
 };
